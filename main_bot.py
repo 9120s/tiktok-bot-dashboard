b77a542d32f1c8bb60d79b0b7072b5da69b70bee
9120s <sol768860@gmail.com>
2026-09-02 14:15:21 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -285,7 +285,7 @@ def run_discord_bot():
             bot.run(BOT_TOKEN)
         except Exception as e:
             print(f"[Discord Bot Error] {e}") 
-    if __name__ == '__main__':
-    threading.Thread(target=run_discord_bot, daemon=True).start()
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    if __name__ == '__main__': 
+        threading.Thread(target=run_discord_bot, daemon=True).start()
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host='0.0.0.0', port=port)
